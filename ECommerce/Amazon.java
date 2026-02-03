510761a2809cbb5c920d4ef4c9fa4f6f13e4a930
Yogesh Ingulkar <yogeshingulkar750@gmail.com>
2026-02-04 00:02:02 +0530

    Update Amazon.java

--- ECommerce/Amazon.java
+++ ECommerce/Amazon.java
@@ -1,16 +1,10 @@
 package ECommerce;
-
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Amazon {
-
 	public static void main(String[] args) {
-			
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		driver.close();
-		
 	}
-
 }
